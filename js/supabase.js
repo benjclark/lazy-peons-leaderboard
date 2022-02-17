a700bc8dc26ec89ca9f1f5deccba144469b2d9a5
Ben Clark <hello@clarkben.com>
2022-02-17 16:12:24 +0000

    Revert "Uses env variables for Supabase auth"
    This reverts commit f58ba182

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,7 @@
 // SUPABASE STUFF
 
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
-const SUPABASE_URL = process.env.SUPABASE_URL;
+// this is horrendous - we'll sort it out later
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0Mzk4ODk1MSwiZXhwIjoxOTU5NTY0OTUxfQ.G8U18_64dtNMktbAawqijsvEYZQh-b9zijQEmBwu7VM'
+const SUPABASE_URL = 'https://dlseueskfqnglzcjbids.supabase.co'
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
